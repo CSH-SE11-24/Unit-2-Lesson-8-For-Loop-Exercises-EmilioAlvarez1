9fb2056f1447534e2643c4295fb3ea9f094f122f
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 18:51:02 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,58 @@
+// Task 1: Print Numbers
+// Write a JavaScript program that uses a for loop to print all numbers from 1 to 10 to the console.
+for(let x = 1; x <= 10; x++){
+  console.log(x)
+}
+
+
+
+
+// Task 2: Odd Numbers
+// Write a JavaScript program that uses a for loop to print all odd numbers from 15 to 40 to the console.
+
+for(let i = 15; i < 40; i+=2){
+  console.log(i)
+}
+
+
+// Task 3: Sum of Numbers
+// Write a JavaScript program that calculates and prints the sum of all numbers from 10 to 20 using a for loop.
+let y = 0
+for(let k = 10; k <= 20; k++){
+   y = y + k
+  console.log(y)
+}
+
+
+
+// Task 4: Array Iteration
+// Create an array of numbers (e.g., [1, 2, 3, 4, 5]) and write a JavaScript program that uses a for loop to iterate through the array and print each element to the console.
+let array = [1,2,3,4,5]
+for(let u = 0; u < array.length ; u++){
+ console.log(array[u])
+}
+
+
+
+
+// Task 5: Array Sum
+// Create an array of numbers and write a JavaScript program that calculates and prints the sum of all the elements in the array using a for loop.
+
+let numbers = [2,4,7,11,16,22]
+for(let h = 0; h < numbers.length; h++){
+  console.log(numbers[Math.random] + numbers[Math.random])
+}
+
+
+
+
+
+// Stretch: Element counter
+// Create an array of numbers
+// Prompt the user for a number and write a for loop that tracks how many times that number is in the array
+// e.g. 5 for [5, 1, 2, 5] --> 2
+
+
+
+
+
